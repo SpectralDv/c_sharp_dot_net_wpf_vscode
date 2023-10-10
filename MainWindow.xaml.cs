67e498f5652117c954e7c87ca30698cf1f9c20a9
SpectralDv <z2a0c1k1@gmail.com>
2023-10-10 22:41:30 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach(UIElement el in MainGrid.Children)
+            {
+                if(el is Button)
+                {
+                    ((Button)el).Click += ButtonClick;
+                }
+            }
+        }
+
+        private void ButtonClick(object sender,RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            TextLable1.Text = str;
+        }
+
+        private void ButtonClickWindow(object sender, RoutedEventArgs e)
+        {
+            MainWindow2 taskWindow = new MainWindow2();
+            taskWindow.Show();
+        }
+    }
+
+
+}
